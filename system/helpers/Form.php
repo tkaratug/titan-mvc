5d65e10942f00ada74cba830da2c35284662a927
Turan Karatuğ <tkaratug@hotmail.com.tr>
2016-10-19 21:46:10 +0300

    v1.1.1 rc7

--- system/helpers/Form.php
+++ system/helpers/Form.php
@@ -554,7 +554,11 @@ if ( ! function_exists('form_close') ) {
 if ( ! function_exists('csrf_generate') ) {
     function csrf_generate()
     {
-        return $_SESSION['titan_token'] = base64_encode(openssl_random_pseudo_bytes(32));
+        $titan = Loader::getInstance();
+        $titan->plugin('session');
+        $titan->session->set('titan_token', base64_encode(openssl_random_pseudo_bytes(32)));
+        return $titan->session->get('titan_token');
+        
     }
 }
 
@@ -566,8 +570,11 @@ if ( ! function_exists('csrf_generate') ) {
 if ( ! function_exists('csrf_check') ) {
     function csrf_check($token)
     {
-        if (isset($_SESSION['titan_token']) && $token === $_SESSION['titan_token']) {
-            unset($_SESSION['titan_token']);
+        $titan = Loader::getInstance();
+        $titan->plugin('session');
+        
+        if ($titan->session->is_exists('titan_token') && $token === $titan->session->get('titan_token')) {
+            $titan->session->delete('titan_token');
             return true;
         }
         return false;
